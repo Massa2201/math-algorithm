8a260f8a0ef70e8c88ac116ab939850b0c68bd69
Imai Masashi <chrono.trigger.2424@gmail.com>
2022-01-30 13:59:37 +0900

    最大公約数の出力

---
+++ code/chapter3/016.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+ll g(ll a, ll b) {
+  while (a >= 1 && b >= 1) {
+    if (a < b) b = b % a;
+    else a = a % b;
+  }
+  return max(a, b);
+  
+}
+
+int main() {
+  ll n, a, b, c;
+  cin >> n;
+  vector<int> vec(n);
+
+  for (int i = 0; i < n; i++) cin >> vec.at(i);
+    a = g(vec.at(0), vec.at(1));
+  for (int i = 0; i < n; i++) {
+    a = g(vec.at(i), a);
+  }
+  cout << a<< endl;
+
+}
+
+
+/*
+
+2次元配列 縦横 3 × 4
+vector<vector<int>> data(3, vector<int>(4));
+
+vector dp(n + 1, vector<int>(9));
+
+vector<int> vec(N);
+
+2次元配列 N x N 中に'-'を入れている
+vector<vector<char>> table(N, vector<char>(N, '-'));
+
+*/
